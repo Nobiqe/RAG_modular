68ef7fc58feae955ee49e369a3df067305ed9b6e
nobiqe <ebrahimnobiqe@gmail.com>
2026-04-21 17:19:50 +0330

    feat(/upload):added upload feature to our fast api

--- backend-api/app/main.py
+++ backend-api/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
-
+from  app.worker import process_pdf_task
 app = FastAPI(title="Enhanced RAG API", version="1.0.0")
 
 @app.get("/")
 async def read_root(): 
     return {"status": "success", "message": "Welcome to the Enhanced RAG API!"}
+
+@app.post("/upload")
+def upload_document(filename: str):
+    # The .delay() method is Celery's magic command to send the task to Redis
+    task = process_pdf_task.delay(filename)
+    
+    return {
+        "message": "File received. Processing in the background!",
+        "filename": filename,
+        "task_id": task.id
+    }
